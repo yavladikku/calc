2bb660f836c1d16e8cc00e37afaa0e907088dd9d
yavladikku <yavladikku@gmail.com>
2021-05-22 15:13:25 +0300

    basic

---
+++ calc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, op;
+    double result;
+
+    cout << "First number\n";
+    cin >> a;
+    cout << "Second number\n";
+    cin >> b;
+    cout << "Operation\n1 +\n2 -\n3 *\n4 /";
+    cin >> op;
+
+    switch (op) {
+        case 1:
+        //code
+        break;
+        case 2:
+        //code
+        break;
+        case 3:
+        //code
+        break;
+        case 4:
+        //code
+        break;
+    }
+
+    cout << result;
+}
